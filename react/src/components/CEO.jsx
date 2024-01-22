0f9d208132d8b224708d64098ebf89328dbeddec
Mathias Dziurkowski <128103391+MathiasDziurkowski@users.noreply.github.com>
2024-01-22 12:57:16 -0300

    v0.1

--- react/src/components/CEO.jsx
+++ react/src/components/CEO.jsx
@@ -20,8 +20,8 @@ export default function CEO() {
                 <div className="flex flex-col align-middle absolute pl-36 md:relative justify-center gap-32 md:w-ceo">
                     <h1 className="font-raleway md:text-4xl">“It`s easy & simple using finpay bank. We are able to manage our finances to seamlessly, no cap”</h1>
                     <div>
-                        <h1 className="font-raleway pl-16 text-sm font-extrabold md:text-base md:pl-0">Ballapen Jack</h1>
-                        <p className="font-raleway pl-16 text-sm text-slate-400 md:text-base md:pl-0">CEO Finpay</p>
+                        <h1 className="font-raleway  text-sm font-extrabold md:text-base md:pl-0">Ballapen Jack</h1>
+                        <p className="font-raleway  text-sm text-slate-400 md:text-base md:pl-0">CEO Finpay</p>
                     </div>
                 </div>
                 <div className="flex items-end justify-end gap-12 p-6 md:p-12">
